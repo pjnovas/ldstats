ad809c5273cd049ec115a2940604be87e5919975
Pablo Novas <bairesdev_pnovas@iseatz.com>
2017-08-22 19:25:32 -0300

    Added LD 39

--- test/ludumdare/integration.js
+++ test/ludumdare/integration.js
@@ -1,15 +1,18 @@
+/* eslint-disable no-console */
+
 // Uncomment for testing integration with the real api
 
 // const LudumDareAPI = require('lib/ludumdare')
 
 describe('LudumDareAPI#integration', () => {
-  it('should fetch ludum dares by username' /*, done => {
+  it('should fetch ludum dares by username'/*, done => {
     let ld = new LudumDareAPI()
 
     ld.user('pjnovas')
       .then(ld.userEntries)
       .then(ld.fulfillEntries)
-      .then(console.log) // eslint-disable-line no-console
+      .then(console.log)
       .then(done)
+      .catch(console.log)
   }*/)
 })
