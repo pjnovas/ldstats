76db1fe0b948a873b6b99f159d73c83d340243f3
Pablo Novas <pjnovas@gmail.com>
2020-05-16 17:45:33 -0300

    Dockerize (#43)
    * dockerized

--- config/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Expose the configuration. Uses the test config if NODE_ENV env setting
- * is set as 'test'. Otherwise it uses the default config file
- */
-
-import config from './config.json';
-export default config;
